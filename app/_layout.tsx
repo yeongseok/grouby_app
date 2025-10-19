08796a01243ee799f16929c4c04c4a589b032858
lakenear2 <lakenear2@gmail.com>
2025-10-19 20:01:57 +0900

    empty_project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,9 +7,5 @@ export default function RootLayout() {
       name="(tabs)"
       options={{ headerShown: false}}
     />
-    <Stack.Screen
-      name="movie/[id]"
-      options={{ headerShown: false}}
-    />
   </Stack>;
 }
